4629931aafd5dc67db1a2c05ba5311ab85b68273
Ariefsoma <98167698+Ariefsoma@users.noreply.github.com>
2025-10-22 13:05:39 +0700

    Add files via upload

---
+++ app/Models/Guru.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Guru extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nip',
+        'nama',
+        'mata_pelajaran',
+        'email',
+        'telepon',
+    ];
+}
